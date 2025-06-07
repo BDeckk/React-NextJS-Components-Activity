6d836f5386d0004957147e0dbd5f99686cc8c5f9
Benideck Longakit <longakitbenideck@gmail.com>
2025-06-07 23:11:29 +0800

    [Features][Longakit] Final Activity
    Final Part A and Part B. V.2

--- part-b-react-components/pages/index.js
+++ part-b-react-components/pages/index.js
@@ -6,18 +6,21 @@ export default function Home() {
   return (
 
     <main style={{ padding: '20px' }}>
+      <div style={{border: "2px solid #0B1D51", background: "#393E46"}}>
       <WelcomeCard name="Benideck" /> {/* calls WelcomeCard.jsx*/}
+      </div>
 
       <div style={{border: "2px solid #0B1D51",
       padding: "20px",
       marginTop: "10px",
-      borderRadius: "10px"}}>
+      borderRadius: "10px", 
+      background: "#222831"}}>
 
       <Counter />{/* calls Counter.jsx*/}
        <StudentInfo />{/* calls StudentInfo.jsx*/}
        
       </div>
-      
+
     </main>
   );
 }
